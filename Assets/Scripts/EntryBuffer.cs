e4988c99ff8c6936e5a533b54b8769cf0557eaf6
Maxwell300 <schwartzm1231@gmail.com>
2026-04-20 00:31:20 -0400

    Entry Buffer outline

---
+++ Assets/Scripts/EntryBuffer.cs
@@ -0,0 +1,22 @@
+using NUnit.Framework;
+using UnityEngine;
+using System.Collections.Generic;
+
+
+public class EntryBuffer : MonoBehaviour
+{
+    List<GameObject> entryBoxes = new List<GameObject>();
+    public GameObject[] boxTypes;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    { 
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
