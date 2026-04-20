81fd74052e8782fda939b71f9bac09f92867adf5
Maxwell300 <schwartzm1231@gmail.com>
2026-04-20 01:07:15 -0400

    Updates to entry conveyor belt script

--- Assets/Scripts/EntryBuffer.cs
+++ Assets/Scripts/EntryBuffer.cs
@@ -6,17 +6,34 @@ using System.Collections.Generic;
 public class EntryBuffer : MonoBehaviour
 {
     List<GameObject> entryBoxes = new List<GameObject>();
+    public GameObject[] entryTargets;
     public GameObject[] boxTypes;
+    public float timer = 60f;
+
+    private bool busy = false;
+    private int x = 0;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
-    { 
+    {
         
+        //Spawn box
+        //Instantiate(boxTypes[x], entryTargets[0].transform.position, Quaternion.identity);
+
+        //move box quickly from path to path via children
+        //once it reaches Path5 it will teleport to the first available slot
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (busy == false)
+        {
+            busy = true;
+            GlobalVariables.Timer(ref busy, ref timer);
+            Instantiate(boxTypes[x], entryTargets[0].transform.position, Quaternion.identity);         
+        } 
+
         
     }
 }
