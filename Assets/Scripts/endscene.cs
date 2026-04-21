f59d3f0cf639b263ffd9e2be510ad09b37b9a928
Austin Leach <austinleach850@gmail.com>
2026-04-20 20:01:55 -0400

    endscene script

---
+++ Assets/Scripts/endscene.cs
@@ -0,0 +1,29 @@
+using TMPro;
+using UnityEngine;
+
+public class totalCoinsAtEnd : MonoBehaviour
+{
+    [Header("UI")]
+    public TextMeshProUGUI totalscoretext; // Assign your TMP text component in inspector
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        // Update the text with the global total collected value when scene loads
+        UpdateTotalCollectedDisplay();
+    }
+
+    private void UpdateTotalCollectedDisplay()
+    {
+        if (totalscoretext != null)
+        {
+            totalscoretext.text = totalscoretext.text + GlobalVariables.score.ToString();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
